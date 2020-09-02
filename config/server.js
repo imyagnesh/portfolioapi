14ac0a7369008b6e0524d042157345ee3287caf3
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro-2.local>
2020-09-02 21:44:19 +0530

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b470d1c440d99d6673dddcd3689159bf'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3e24540f-ac9e-4b25-9bd6-47620c157c1c'
+};
